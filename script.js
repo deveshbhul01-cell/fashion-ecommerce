d63762ab07e63234b9fe5831d735a146bb6c5c7f
deveshbhul01-cell <deveshbhul01@gmail.com>
2026-08-21 03:43:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let cart = [];
+
+function addToCart(productName, price) {
+    cart.push({
+        name: productName,
+        price: price
+    });
+
+    alert(productName + " added to cart!");
+
+    console.log(cart);
+}
