36ed86621d0822681cddc34040b5dabfa19d4a17
deveshbhul01-cell <deveshbhul01@gmail.com>
2026-08-25 02:03:28 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://yrbafbqzbbvshauxfuki.supabase.co";
 
-const SUPABASE_ANON_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_ANON_KEY = "YOUR_PUBLISHABLE_KEY_HERE";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
